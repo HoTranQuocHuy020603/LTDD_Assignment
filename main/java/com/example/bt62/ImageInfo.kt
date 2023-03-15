3bd9682fd0dac07d2f416086be47987b1f2e6e1f
Huy Archive <89513083+HoTranQuocHuy020603@users.noreply.github.com>
2023-03-15 12:54:34 +0700

    Delete main directory

--- main/java/com/example/bt62/ImageInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bt62
-
-import android.os.Parcel
-import android.os.Parcelable
-
-data class ImageInfo(var article_id:Int, var article_title: String, var article_image: String,
-                     var article_description: String
-){}
